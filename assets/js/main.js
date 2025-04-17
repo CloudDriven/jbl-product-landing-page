0a5d147d45568ca96e2a75870229986048e64332
Pedro Duarte <pedroeclaudioduarte@gmail.com>
2025-04-17 19:27:30 +0100

    project setup, header & Nav Menu

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/*==================== SHOW MENU ====================*/
+const navMenu = document.querySelector('.navMenu'),
+      navToggle = document.querySelector('.navToggle'),
+      navClose = document.querySelector('.navClose')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.navLink')
+
+function linkAction(){
+    const navMenu = document.querySelector('.nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
